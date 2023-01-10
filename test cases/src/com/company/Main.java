b7643b2838ce15353df1d597c6446ea824b9e72c
777ufc <116823173+777ufc@users.noreply.github.com>
2023-01-10 11:32:45 +0600

    Add files via upload

---
+++ test cases/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myName;
+        myName = "Ермек";
+        System.out.println(myName);
+        int age;
+        age = 18;
+        System.out.println(age);
+        String myCity;
+        myCity = "Бишкек";
+        System.out.println(myName);
+        System.out.println(age);
+        System.out.println(myCity);
+	// write your code here
+    }
+}
